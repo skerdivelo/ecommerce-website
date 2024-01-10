63c4059582402c02a2240b901e0aa4654f67985a
skerdivelo <skerdivelo42@gmail.com>
2024-01-10 19:08:29 +0100

    Update product list

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -1,47 +1,42 @@
 export const products = [
-  {
-    name: "Macbook Pro",
-    category: "Laptops",
-    dicount: "$1,899",
-    price: "$2,199",
-    rating: 4.7,
-    image:
-      "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?auto=format&fit=crop&q=80&w=2970&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Watch SE",
-    category: "Watches",
-    dicount: "$199",
-    price: "$229",
-    rating: 4.8,
-    image:
-      "https://images.unsplash.com/photo-1624096104992-9b4fa3a279dd?auto=format&fit=crop&q=80&w=2638&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "AirPods",
-    category: "Headphones",
-    dicount: "$299",
-    price: "$329",
-    rating: 4.9,
-    image:
-      "https://images.unsplash.com/photo-1591370264374-9a5aef8df17a?auto=format&fit=crop&q=80&w=3087&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "iPhone 15",
-    category: "Phones",
-    dicount: "$1099",
-    price: "$1299",
-    rating: 4.6,
-    image:
-      "https://images.unsplash.com/photo-1695048064926-4da0dd8259d1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Samsung QLED TV",
-    category: "TVs",
-    dicount: "$1,299",
-    price: "$1,499",
-    rating: 4.5,
-    image:
-      "https://images.unsplash.com/photo-1602522660406-8cc77767d783?auto=format&fit=crop&q=80&w=2738&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
+    {
+        name: "Macbook Pro",
+        category: "Laptops",
+        dicount: "$1,899",
+        price: "$2,199",
+        rating: 4.7,
+        image: "https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?auto=format&fit=crop&q=80&w=2970&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Watch SE",
+        category: "Watches",
+        dicount: "$199",
+        price: "$229",
+        rating: 4.8,
+        image: "https://images.unsplash.com/photo-1624096104992-9b4fa3a279dd?auto=format&fit=crop&q=80&w=2638&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "AirPods",
+        category: "Headphones",
+        dicount: "$299",
+        price: "$329",
+        rating: 4.9,
+        image: "https://images.unsplash.com/photo-1591370264374-9a5aef8df17a?auto=format&fit=crop&q=80&w=3087&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "iPhone 15",
+        category: "Phones",
+        dicount: "$1099",
+        price: "$1299",
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1695048064926-4da0dd8259d1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "iMac",
+        category: "Computer",
+        dicount: "$1,299",
+        price: "$1,499",
+        rating: 4.5,
+        image: "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
 ];
