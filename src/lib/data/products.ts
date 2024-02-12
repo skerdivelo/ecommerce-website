bfe946e9e1e3ccaf6eba73d6431db5867f44264c
skerdivelo <skerdivelo42@gmail.com>
2024-02-12 12:00:58 +0100

    Aggiunti nuovi prodotti alla lista

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -44,4 +44,13 @@ export const products = [
         rating: 4.5,
         image: "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
+    {
+        id: 6,
+        name: "Galaxy Watch",
+        category: "Orologi",
+        dicount: "$549",
+        price: "$599",
+        rating: 4.1,
+        image: "https://images.unsplash.com/photo-1553545204-4f7d339aa06a?q=80&w=1993&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
 ];
